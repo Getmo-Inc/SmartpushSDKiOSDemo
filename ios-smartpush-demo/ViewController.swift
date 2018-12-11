165b5a4e219815a98576c36fe3c9c241aa38c41f
Carlos Corrêa <carlscorrea@gmail.com>
2018-12-11 14:59:03 -0200

    Initial Commit

---
+++ ios-smartpush-demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-smartpush-demo
+//
+//  Created by Carlos Correa on 11/12/18.
+//  Copyright © 2018 Carlos Correa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
